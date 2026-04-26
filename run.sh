1d3f7cd748017cc9ec63f901766e763d3bbf07d5
rolf <snippo3@ej-bap.de>
2026-04-26 12:20:08 +0200

    .

--- run.sh
+++ run.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
 
-set -e
+function fehler() {
+  echo ">>> FEHLER!"
+  echo ""
+  sleep 10
+  exit 1
+}
 
 echo "Start Piggy-Bank"
 
-source .venv/bin/activate
-pip install -e .
+cd ${HOME}/git/github/piggy-bank-bg || fehler
+source .venv/bin/activate || fehler
+pip install -e . || fehler
 
 
 echo "Initialisiere das Kamerasystem"
-./setup_vcam.sh
+./setup_vcam.sh || fehler
 
 echo "####################################"
 echo "### Starte die Konferenzkamera:"
+echo "### Anschauen kann man sich das auch:"
+echo "### VLC starten und dann dort unter"
+echo "###    Medien"
+echo "###    -> Aufnahmegeraet oeffnen"
+echo "###    -> Video-Geraet /dev/video10"
 echo "####################################"
-python -m bgnd --camera 0
+python -m bgnd --camera 0 || fehler
 
 echo "Fertig."
 sleep 3
